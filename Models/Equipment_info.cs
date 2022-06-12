c3c7221b468971cf612854d5fd3499427c669441
NHH-Viet <kingtung93@gmail.com>
2022-06-13 00:15:52 +0700

    them mot dong du lieu vao cai bang lone

--- Models/Equipment_info.cs
+++ Models/Equipment_info.cs
@@ -48,10 +48,11 @@ namespace projectE.Models
         public Nullable<short> R_Scl_Dex { get; set; }
         public Nullable<short> R_Scl_Int { get; set; }
         public Nullable<short> R_Scl_Fai { get; set; }
-        public string SKill_Cost { get; set; }
         public string ET_ID { get; set; }
         public string Scl_Arc { get; set; }
         public Nullable<short> R_Scl_Arc { get; set; }
+        public string SKill_Cost { get; set; }
+        public string SNames { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Classes_build> Classes_build { get; set; }
